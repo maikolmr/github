fa0542396ccfdf9c5186a63871ecd31b02e9d3b6
Maikol Magalhães Rodrigues <maikol.rodrigues@serpro.gov.br>
2021-05-02 15:35:50 -0300

    atualizacao de notacao lambda

--- cursoJava14/src/lambdas/Lambda1.java
+++ cursoJava14/src/lambdas/Lambda1.java
@@ -1,4 +1,7 @@
 package lambdas;
+
+import java.util.function.BinaryOperator;
+
 public class Lambda1 {
 
 	public static void main(String[] args) {
@@ -6,6 +9,12 @@ public class Lambda1 {
 		System.out.println("teste");
 		Runnable tarefa = () -> System.out.println("Hello!");
 		tarefa.run();
+		
+		BinaryOperator<Double> calc = (a,b) -> a+b;
+		Double res = calc.apply(3.0, 4.0);
+		
+		System.out.println(res);
+		
 
 	}
 
