e386ca6cde904d830548cd772baf92c37c7df49b
Maikol Magalhães Rodrigues <maikol.rodrigues@serpro.gov.br>
2021-05-09 20:05:26 -0300

    exemplos de javascript

---
+++ curso_javascript_funcional/funcoes/arrow.js
@@ -0,0 +1,9 @@
+const subtrair = (a,b) => a-b;
+
+console.log(subtrair(5,2))
+
+const exp = (a,b) => Math.pow(a,b)
+
+const exp2 = (a,b) => Math.pow(a,b)
+
+console.log(exp(2,4))
---
+++ curso_javascript_funcional/funcoes/ex1.js
@@ -0,0 +1,13 @@
+//Function Declaration
+function bomDia() {
+    console.log('Bom Dia!')
+  }
+  
+  bomDia()
+  //Function expression
+  const boaTarde = function() {
+    console.log('Boa Tarde!')
+  }
+  
+  boaTarde()
+
